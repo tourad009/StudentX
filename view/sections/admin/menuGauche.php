600cae71253e6ea2d28f0e9aab2c0672a99921e3
Tourad <tourad959@gmail.com>
2025-03-01 10:54:17 +0000

    Ajout de gitkeep pour garder la structure des dossiers

--- view/sections/admin/menuGauche.php
+++ view/sections/admin/menuGauche.php
@@ -2,6 +2,7 @@
   <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
     <div class="sb-sidenav-menu">
       <div class="nav">
+        <!-- Dashboard -->
         <div class="sb-sidenav-menu-heading">Core</div>
         <a class="nav-link" href="index.html">
           <div class="sb-nav-link-icon">
@@ -9,128 +10,47 @@
           </div>
           Dashboard
         </a>
-        <div class="sb-sidenav-menu-heading">Interface</div>
-        <a
-          class="nav-link collapsed"
-          href="#"
-          data-bs-toggle="collapse"
-          data-bs-target="#collapseLayouts"
-          aria-expanded="false"
-          aria-controls="collapseLayouts"
-        >
+
+        <!-- Gestion des Étudiants -->
+        <div class="sb-sidenav-menu-heading">Gestion des Étudiants</div>
+        <a class="nav-link" href="liste_etudiants.html">
           <div class="sb-nav-link-icon">
-            <i class="fas fa-columns"></i>
+            <i class="fas fa-users"></i>
           </div>
-          Layouts
-          <div class="sb-sidenav-collapse-arrow">
-            <i class="fas fa-angle-down"></i>
-          </div>
+          Liste des étudiants
         </a>
-        <div
-          class="collapse"
-          id="collapseLayouts"
-          aria-labelledby="headingOne"
-          data-bs-parent="#sidenavAccordion"
-        >
-          <nav class="sb-sidenav-menu-nested nav">
-            <a class="nav-link" href="layout-static.html"
-              >Static Navigation</a
-            >
-            <a class="nav-link" href="layout-sidenav-light.html"
-              >Light Sidenav</a
-            >
-          </nav>
-        </div>
-        <a
-          class="nav-link collapsed"
-          href="#"
-          data-bs-toggle="collapse"
-          data-bs-target="#collapsePages"
-          aria-expanded="false"
-          aria-controls="collapsePages"
-        >
+
+        <!-- Gestion des Évaluations -->
+        <div class="sb-sidenav-menu-heading">Gestion des Évaluations</div>
+        <a class="nav-link" href="liste_evaluations.html">
           <div class="sb-nav-link-icon">
-            <i class="fas fa-book-open"></i>
+            <i class="fas fa-list"></i>
           </div>
-          Pages
-          <div class="sb-sidenav-collapse-arrow">
-            <i class="fas fa-angle-down"></i>
-          </div>
+          Liste des évaluations
         </a>
-        <div
-          class="collapse"
-          id="collapsePages"
-          aria-labelledby="headingTwo"
-          data-bs-parent="#sidenavAccordion"
-        >
-          <nav
-            class="sb-sidenav-menu-nested nav accordion"
-            id="sidenavAccordionPages"
-          >
-            <a
-              class="nav-link collapsed"
-              href="#"
-              data-bs-toggle="collapse"
-              data-bs-target="#pagesCollapseAuth"
-              aria-expanded="false"
-              aria-controls="pagesCollapseAuth"
-            >
-              Authentication
-              <div class="sb-sidenav-collapse-arrow">
-                <i class="fas fa-angle-down"></i>
-              </div>
-            </a>
-            <div
-              class="collapse"
-              id="pagesCollapseAuth"
-              aria-labelledby="headingOne"
-              data-bs-parent="#sidenavAccordionPages"
-            >
-              <nav class="sb-sidenav-menu-nested nav">
-                <a class="nav-link" href="login.html">Login</a>
-                <a class="nav-link" href="register.html">Register</a>
-                <a class="nav-link" href="password.html"
-                  >Forgot Password</a
-                >
-              </nav>
-            </div>
-            <a
-              class="nav-link collapsed"
-              href="#"
-              data-bs-toggle="collapse"
-              data-bs-target="#pagesCollapseError"
-              aria-expanded="false"
-              aria-controls="pagesCollapseError"
-            >
-              Error
-              <div class="sb-sidenav-collapse-arrow">
-                <i class="fas fa-angle-down"></i>
-              </div>
-            </a>
-            <div
-              class="collapse"
-              id="pagesCollapseError"
-              aria-labelledby="headingOne"
-              data-bs-parent="#sidenavAccordionPages"
-            >
-              <nav class="sb-sidenav-menu-nested nav">
-                <a class="nav-link" href="401.html">401 Page</a>
-                <a class="nav-link" href="404.html">404 Page</a>
-                <a class="nav-link" href="500.html">500 Page</a>
-              </nav>
-            </div>
-          </nav>
-        </div>
-        <div class="sb-sidenav-menu-heading">Addons</div>
-        <a class="nav-link" href="charts.html">
+
+        <!-- Gestion des Notes -->
+        <div class="sb-sidenav-menu-heading">Gestion des Notes</div>
+        <a class="nav-link" href="liste_notes.html">
           <div class="sb-nav-link-icon">
-            <i class="fas fa-chart-area"></i>
+            <i class="fas fa-file-alt"></i>
           </div>
-          Charts
+          Liste des notes
         </a>
-        <a class="nav-link" href="tables.html">
-          <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-          Tables
+
+        <!-- Paramètres -->
+        <div class="sb-sidenav-menu-heading">Paramètres</div>
+        <a class="nav-link" href="gestion_utilisateurs.html">
+          <div class="sb-nav-link-icon">
+            <i class="fas fa-cogs"></i>
+          </div>
+          Gestion des utilisateurs
+        </a>
+        <a class="nav-link" href="changer_mot_de_passe.html">
+          <div class="sb-nav-link-icon">
+            <i class="fas fa-lock"></i>
+          </div>
+          Changer mot de passe
         </a>
       </div>
     </div>
